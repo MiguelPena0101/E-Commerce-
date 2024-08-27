e35db0a95eb0332d4a0570fe233e411ab8070af3
Rey <Miguel.Pena1709@gmail.com>
2024-08-27 19:53:17 -0400

    add import of sequelize connection

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const routes = require('./routes');
 const sequelize = require('sequelize');
 // import sequelize connection
 
+const sequelize = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
