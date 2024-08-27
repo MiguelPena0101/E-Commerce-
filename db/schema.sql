8a2811e5330e114ffcae43dcbb1cbe3760d82334
Rey <Miguel.Pena1709@gmail.com>
2024-08-27 19:17:21 -0400

    database structure created

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,28 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+\c ecommerce_db;
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INTEGER NOT NULL DEFAULT 10,
+    category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL
+);
+
+CREATE TABLE tags (
+    id SERIAL PRIMARY KEY,
+    tag_name VARCHAR(255)
+);
+
+CREATE TABLE product_tags (
+    id SERIAL PRIMARY KEY,
+    product_id INTEGER REFERENCES products(id) ON DELETE CASCADE,
+    tag_id INTEGER REFERENCES tags(id) ON DELETE CASCADE
+);
